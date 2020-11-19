100653ef69c91fdc29ce37b5d62ac47f7cf5ddd6
Dossja <domka_24@interia.pl>
2020-11-19 21:12:24 +0100

    Tetris blocks with random colors on SpaceBar.

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] cubes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Color color = Random.ColorHSV();
+            foreach (GameObject cube in cubes)
+            {
+                Renderer cubeRenderer = cube.GetComponent<Renderer>();
+                cubeRenderer.material.SetColor("_Color", color);
+            }
+        }
+    }
+}
